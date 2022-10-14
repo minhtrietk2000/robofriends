209de4b7eb44b5bcd08c2b682464219dc487bf83
minhtrietk2000 <48998009+minhtrietk2000@users.noreply.github.com>
2022-10-14 16:25:53 +0700

    Updates

---
+++ precache-manifest.820cb9d898de14d57deacc31b23d8d21.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b63c249bc235a0d6564",
+    "url": "/robofriends/static/css/main.0fc8afef.chunk.css"
+  },
+  {
+    "revision": "1b63c249bc235a0d6564",
+    "url": "/robofriends/static/js/main.1b63c249.chunk.js"
+  },
+  {
+    "revision": "84cf99745ce0980187aa",
+    "url": "/robofriends/static/js/1.84cf9974.chunk.js"
+  },
+  {
+    "revision": "330384d8b57e250a01d7",
+    "url": "/robofriends/static/css/2.ff734f29.chunk.css"
+  },
+  {
+    "revision": "330384d8b57e250a01d7",
+    "url": "/robofriends/static/js/2.330384d8.chunk.js"
+  },
+  {
+    "revision": "6bf665704170461feaf8",
+    "url": "/robofriends/static/js/runtime~main.6bf66570.js"
+  },
+  {
+    "revision": "a1887a6560a509f11822208c8e5651e7",
+    "url": "/robofriends/static/media/Okeffe-8MK10.a1887a65.woff"
+  },
+  {
+    "revision": "e413b6cfb043918a5dd5644f57aecd0c",
+    "url": "/robofriends/index.html"
+  }
+];
